5a572210ce016010ad51e0a0b4f7c32408f061b6
Ronghui Mu <41231651+mrh996@users.noreply.github.com>
2026-01-02 23:22:08 +0000

    Delete prediction_model directory

--- prediction_model/common/code/test.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-import pandas as pd
-data = pd.read_csv('/LOCAL2/mur/MRH/protein_RL/prediction_model/common/code/5_clean_name.txt', sep=' ', header=None)
-data_npy = data.values
-np.save('large_data.npy', data_npy)
